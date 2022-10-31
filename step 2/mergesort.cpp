fbc1704cd47a13bf7f8bee27b526174839d0fc94
Sajib Hossain <sajibhossain1185@gmail.com>
2022-10-31 13:23:31 +0600

    learned merge sort

---
+++ step 2/mergesort.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+using namespace std;
+
+void merge(int arr[], int start, int mid, int end)
+{
+    int i = start;
+    int j = mid + 1;
+    int k = start;
+    int b[100000];
+
+    while (i <= mid && j <= end)
+    {
+        if (arr[i] < arr[j])
+        {
+            b[k++] = arr[i++];
+        }
+        else
+        {
+            b[k++] = arr[j++];
+        }
+    }
+
+    if (i > mid)
+    {
+        while (j <= end)
+        {
+            b[k++] = arr[j++];
+        }
+    }
+    else
+    {
+        while (i <= mid)
+        {
+            b[k++] = arr[i++];
+        }
+    }
+
+    for (k = start; start <= end; k++)
+    {
+        arr[k] = b[k];
+    }
+}
+
+void mergeSort(int arr[], int start, int end)
+{
+    if (start > end)
+    {
+
+        int mid = start + (end - start) / 2;
+
+        mergeSort(arr, start, mid);
+        mergeSort(arr, mid + 1, end);
+        merge(arr, start, mid, end);
+    }
+}
+
+int main()
+{
+    int start = 0;
+    int end = 5;
+    int arr[5] = {5, 1, 7, 2, 4};
+
+    mergeSort(arr, start, end);
+
+    printArray(arr, end);
+
+    return 0;
+}
